202643044481210cbe9c56cce1ade992f033df7f
congsoony <44600941+congsoony@users.noreply.github.com>
2021-08-02 14:32:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.7 MB (100.00%)

---
+++ leetcode-codes/0046-permutations/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> v;
+        sort(nums.begin(),nums.end());
+        do {
+            v.push_back(nums);
+        } while (next_permutation(nums.begin(),nums.end()));
+        return v;
+    }
+};
