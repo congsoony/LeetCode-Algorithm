1bc17e23564dbc8d900a4275338d30b4ed3d3711
congsoony <44600941+congsoony@users.noreply.github.com>
2021-09-16 08:50:33 +0000

    Sync LeetCode submission Runtime - 187 ms (5.04%), Memory - 16.4 MB (99.99%)

---
+++ leetcode-codes/0207-course-schedule/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+	vector<int> v[100001];
+	vector<int> indegree;
+	bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+		indegree = vector<int>(numCourses);
+		queue<int> q;
+		for (vector<int> t : prerequisites) {
+			v[t[1]].push_back(t[0]);
+			indegree[t[0]]++;
+		}
+		for (int i = 0; i < numCourses; i++)
+			if (indegree[i] == 0)q.push(i);
+		
+		while (!q.empty()) {
+			int cur = q.front();
+			q.pop();
+			for (int next : v[cur]) {
+				indegree[next]--;
+				if (indegree[next] == 0)
+					q.push(next);
+			}
+		}
+		for (int i = 0; i < numCourses; i++)
+			if (indegree[i])return false;
+		return true;
+	}
+
+};
