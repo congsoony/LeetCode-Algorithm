5de4520cd2ffc08199f98d2ae313cae6db3a2377
congsoony <44600941+congsoony@users.noreply.github.com>
2021-11-17 07:42:11 +0000

    Sync LeetCode submission Runtime - 4 ms (0.38%), Memory - 9.5 MB (100.00%)

--- leetcode-codes/0074-search-a-2d-matrix/solution.cpp
+++ leetcode-codes/0074-search-a-2d-matrix/solution.cpp
@@ -1,12 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 0; j < matrix[i].size(); j++) {
-                if (matrix[i][j] == target)
-                    return true;
-            }
-        }
-        return false;
-    }
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
+		int n = 0, m = matrix[0].size();
+		int left = 0, right = m - 1;
+		int idx = -1;
+		for (int i = 0; i < matrix.size(); i++) {
+			if (matrix[i][left] <= target && target <= matrix[i][right])
+				idx = i;
+		}
+		if (idx == -1)return false;
+		int t = lower_bound(matrix[idx].begin(), matrix[idx].end(), target)-matrix[idx].begin();
+		if (t >= m)return false;
+		return matrix[idx][t] == target;
+	}
 };
