dbf28af03337267cecb2bbbed8053103d487133c
congsoony <44600941+congsoony@users.noreply.github.com>
2022-03-28 18:11:40 +0000

    Sync LeetCode submission Runtime - 14 ms (0.00%), Memory - 9.7 MB (100.00%)

---
+++ leetcode-codes/0226-invert-binary-tree/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        dfs(root);
+        return root;
+    }
+    void dfs(TreeNode * root){
+        if(!root)return;
+        dfs(root->left);
+        dfs(root->right);
+        TreeNode *l=root->left;
+        TreeNode *r= root->right;
+        root->left=r;
+        root->right=l;
+    }
+};
