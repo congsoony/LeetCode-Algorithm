e8da751b1382180a2319bb74d6917e562a3c51e6
congsoony <44600941+congsoony@users.noreply.github.com>
2022-05-04 13:56:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.5 MB (100.00%)

---
+++ leetcode-codes/0812-rotate-string/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        string t=s+s;
+        for(int i=0;i<s.size();i++){
+            if(t.substr(i,s.size())==goal) return true;
+        }
+        return false;
+        
+    }
+};
