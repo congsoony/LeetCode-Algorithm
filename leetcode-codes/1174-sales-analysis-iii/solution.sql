bb0208f1e4fc37007fce177d2f1b85dc2fe604de
congsoony <44600941+congsoony@users.noreply.github.com>
2023-09-09 06:34:20 +0000

    Sync LeetCode submission Runtime - 2338 ms (5.01%), Memory - 0.0B (100.00%)

---
+++ leetcode-codes/1174-sales-analysis-iii/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select p.product_id,p.product_name
+from product p
+where p.product_id in(
+    select product_id from sales
+    where sale_date between '2019-01-01' and '2019-03-31'
+) and  p.product_id not in(
+    select product_id from sales
+    where sale_date not between '2019-01-01' and '2019-03-31'
+)
+
