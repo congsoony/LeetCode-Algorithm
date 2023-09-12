fac177b126fce6dfe8610eb4b08020fa9c935501
congsoony <44600941+congsoony@users.noreply.github.com>
2023-09-12 15:25:16 +0000

    Sync LeetCode submission Runtime - 869 ms (10.11%), Memory - 0.0B (100.00%)

---
+++ leetcode-codes/1278-product-price-at-a-given-date/solution.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select product_id,first_value(new_price) over(partition by product_id order by change_date desc) price
+from products
+where change_date<= '2019-08-16'
+union
+select product_id,10
+from products
+group by product_id
+having min(change_date)>'2019-08-16'
