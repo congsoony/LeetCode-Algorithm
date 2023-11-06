d4983767bcaea112f1be53af048fc276be5c98ef
congsoony <44600941+congsoony@users.noreply.github.com>
2023-11-06 04:13:54 +0000

    Sync LeetCode submission Runtime - 1195 ms (5.01%), Memory - 0.0B (100.00%)

--- leetcode-codes/2110-employees-with-missing-information/solution.sql
+++ leetcode-codes/2110-employees-with-missing-information/solution.sql
@@ -1,5 +1,11 @@
-/* Write your PL/SQL query statement below */
-select nvl(e.employee_id,s.employee_id) employee_id from employees e
-full outer join salaries s on e.employee_id = s.employee_id
-where e.employee_id is null or s.employee_id is null
+# Write your MySQL query statement below
+with temp as(
+select e.employee_id id1,e.name,s.employee_id id2,s.salary from employees e
+left outer join salaries s on e.employee_id = s.employee_id
+union
+select e.employee_id id1,e.name,s.employee_id id2,s.salary from employees e
+right outer join salaries s on e.employee_id = s.employee_id)
+
+select ifnull(id1,id2) employee_id from temp
+where id1 is null or id2 is null
 order by employee_id
