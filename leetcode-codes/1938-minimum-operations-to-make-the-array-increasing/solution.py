3696b25d258cd635b607402dc54e552b51049c62
congsoony <44600941+congsoony@users.noreply.github.com>
2024-06-11 03:04:05 +0000

    Sync LeetCode submission Runtime - 89 ms (81.16%), Memory - 12.4 MB (100.00%)

---
+++ leetcode-codes/1938-minimum-operations-to-make-the-array-increasing/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def minOperations(self, nums):
+        res = 0
+        for i in range(1,len(nums)):
+            if nums[i-1]>=nums[i]:
+                res+=nums[i-1]-nums[i]+1
+                nums[i]=nums[i-1]+1
+        return res
+
