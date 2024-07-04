38a122e9b0cda730595ef2e4d41d7a0b6db71e30
congsoony <44600941+congsoony@users.noreply.github.com>
2024-07-04 14:30:22 +0000

    Sync LeetCode submission Runtime - 53 ms (71.81%), Memory - 11.5 MB (100.00%)

--- leetcode-codes/2116-count-number-of-pairs-with-absolute-difference-k/solution.py
+++ leetcode-codes/2116-count-number-of-pairs-with-absolute-difference-k/solution.py
@@ -1,9 +1,9 @@
 from collections import Counter
 class Solution(object):
     def countKDifference(self, nums, k):
-        c=Counter(nums)
-        res=0
-        for key,val in c.items():
-            res+=c[key-k]*val
-            res+=c[key+k]*val
+        c = Counter(nums)
+        res = 0
+        for x, val in c.items():
+            res+=c[x+k]*val
+            res+=c[x-k]*val
         return res//2
