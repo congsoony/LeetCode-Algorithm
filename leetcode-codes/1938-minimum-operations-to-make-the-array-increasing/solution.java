083d20b4162d7275b0abf4f62ae9c21f1241affa
congsoony <44600941+congsoony@users.noreply.github.com>
2024-07-10 08:11:11 +0000

    Sync LeetCode submission Runtime - 4 ms (75.93%), Memory - 44.7 MB (100.00%)

---
+++ leetcode-codes/1938-minimum-operations-to-make-the-array-increasing/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int sum = 0;
+        for(int i =1;i<nums.length;i++){
+            if(nums[i-1]>=nums[i]){
+                sum+=nums[i-1]-nums[i]+1;
+                nums[i]=nums[i-1]+1;
+            }
+        }
+        return sum;
+    }
+}
