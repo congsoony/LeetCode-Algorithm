0b5b29fd055ea3135af6de83bae21b67902c5d76
congsoony <44600941+congsoony@users.noreply.github.com>
2024-08-27 11:34:10 +0000

    Sync LeetCode submission Runtime - 682 ms (68.82%), Memory - 0.0B (100.00%)

--- leetcode-codes/0184-department-highest-salary/solution.sql
+++ leetcode-codes/0184-department-highest-salary/solution.sql
@@ -1,8 +1,7 @@
-# Write your MySQL query statement below
-select d.name as Department , e.name as Employee, e.salary
-from employee e
-inner join department d on d.id = e.departmentid
-where (e.salary,e.departmentid) in (
-    select max(e2.salary) salary,e2.departmentid from employee e2
-group by e2.departmentid)
+
+select d1.name as department, e1.name employee ,e1.salary
+from employee e1 inner join department d1 on e1.departmentid = d1.id
+where (e1.salary,e1.departmentid) in (
+    select max(e2.salary) salary ,e2.departmentid from employee e2
+    group by e2.departmentid)
 
