72ae532f3b0cf7eeec0921b55b275ca57bf48658
congsoony <44600941+congsoony@users.noreply.github.com>
2024-10-23 07:48:22 +0000

    Sync LeetCode submission Runtime - 536 ms (100.00%), Memory - 0.0B (100.00%)

--- leetcode-codes/1811-fix-names-in-a-table/solution.sql
+++ leetcode-codes/1811-fix-names-in-a-table/solution.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-
-select user_id, concat(upper(substring(name,1,1)),lower(substring(name,2))) name
-from users
+# Write your MySQL query statement below
+select user_id, concat(upper(substr(name,1,1)),lower(substr(name,2))) name from Users
 order by user_id
