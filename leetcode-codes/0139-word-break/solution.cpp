a26c91bc81f418897ee7bd24f061ed3020346d7e
congsoony <44600941+congsoony@users.noreply.github.com>
2024-10-25 09:58:52 +0000

    Sync LeetCode submission Runtime - 19 ms (15.58%), Memory - 16.9 MB (27.82%)

--- leetcode-codes/0139-word-break/solution.cpp
+++ leetcode-codes/0139-word-break/solution.cpp
@@ -1,22 +1,22 @@
-class Solution {
+//By 콩순이냉장고
+#include<bits/stdc++.h>
+using namespace std;class Solution {
 public:
-vector<int> dp;
-int dfs(string &s,set<string> &m,int idx=0){
-    if(idx>=s.size()) return true;
-
-    int &cache = dp[idx];
-    if(cache!=-1)return cache;
-    cache = 0;
-    for(int i=idx;i<s.size();i++){
-        if(m.count(s.substr(idx,i+1-idx))&&dfs(s,m,i+1)){
-            return cache=true;
+    vector<int> dp;
+    int dfs(string &s,set<string> &m,int cur){
+        int &cache = dp[cur];
+        if(cur>=s.size())return true;
+        if(cache!=-1)return cache;
+        for(int i=cur;i<s.size();i++){
+            if(m.count(s.substr(cur,i-cur+1)) &&dfs(s,m,i+1)){
+                return cache = true;
+            }
         }
+        return cache = false;
     }
-    return cache;
-}
-bool wordBreak(string s, vector<string>& wordDict) {
-    set<string> m(wordDict.begin(),wordDict.end());
-    dp = vector<int>(s.size(),-1);
-    return dfs(s,m);
-}
+    bool wordBreak(string s, vector<string>& wordDict) {
+        set<string> m (wordDict.begin(),wordDict.end());
+        dp = vector<int>(s.size(),-1);
+        return dfs(s,m,0);
+    }
 };
