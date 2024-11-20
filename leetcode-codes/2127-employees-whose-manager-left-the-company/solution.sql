fda265c55885d04be0b01e3d028fe88cc35c3317
congsoony <44600941+congsoony@users.noreply.github.com>
2024-11-20 04:39:06 +0000

    Sync LeetCode submission Runtime - 435 ms (9.96%), Memory - 0.0B (100.00%)

--- leetcode-codes/2127-employees-whose-manager-left-the-company/solution.sql
+++ leetcode-codes/2127-employees-whose-manager-left-the-company/solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select e1.employee_id from employees e1
+select employee_id from employees
 where salary <30000 and manager_id not in (select employee_id from employees)
 order by employee_id
