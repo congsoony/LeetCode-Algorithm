6c7dbc542dd347b0bfd62d7833897da526e632b9
congsoony <44600941+congsoony@users.noreply.github.com>
2025-05-20 07:20:03 +0000

    Sync LeetCode submission Runtime - 413 ms (31.81%), Memory - 0.0B (100.00%)

--- leetcode-codes/0182-duplicate-emails/solution.sql
+++ leetcode-codes/0182-duplicate-emails/solution.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-
 select p.email from person p
+inner join person p2 
+on p.id != p2.id and p.email = p2.email
 group by p.email
-having count(p.email) >1
 
