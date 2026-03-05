56df778fad4f04c95213119243250f8f825e80d4
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-05 00:24:22 +0000

    Sync LeetCode submission Runtime - 392 ms (95.77%), Memory - 0.0B (100.00%)

--- leetcode-codes/0175-combine-two-tables/solution.sql
+++ leetcode-codes/0175-combine-two-tables/solution.sql
@@ -1,2 +1,5 @@
-select p.firstname,p.lastname,a.city,a.state from person p
-left join address a on p.personid = a.personid;
+# Write your MySQL query statement below
+select p.firstName , p.lastName , a.city,a.state
+from person p 
+left join address a on p.personid = a.personid
+
