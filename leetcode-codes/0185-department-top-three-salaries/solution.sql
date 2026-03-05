4c4f3ba9bc7865544a2ca61a1562365e6c01768c
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-05 07:29:03 +0000

    Sync LeetCode submission Runtime - 848 ms (99.85%), Memory - 0.0B (100.00%)

--- leetcode-codes/0185-department-top-three-salaries/solution.sql
+++ leetcode-codes/0185-department-top-three-salaries/solution.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
-with temp as(
-select name,salary,dense_rank() over(partition by departmentid order by salary desc) grade, departmentid from employee)
-select d.name Department,t.name Employee, t.salary Salary
+with temp as(select name, salary, departmentid, dense_rank() over(partition by departmentid order by salary desc) 'ranking' from employee)
+
+
+select d.name department ,t.name employee, t.salary
 from temp t
-inner join department d on d.id = t.departmentid
-where grade<=3
+join department d on d.id = t.departmentid
+where t.ranking<=3
