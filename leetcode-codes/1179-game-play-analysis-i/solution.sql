5a1bb91ab067d6425a0cbb46a4d6019845c7a992
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-06 03:06:00 +0000

    Sync LeetCode submission Runtime - 467 ms (96.60%), Memory - 0.0B (100.00%)

--- leetcode-codes/1179-game-play-analysis-i/solution.sql
+++ leetcode-codes/1179-game-play-analysis-i/solution.sql
@@ -1,4 +1,3 @@
 # Write your MySQL query statement below
-select player_id , min(event_date) first_login
-from activity
+select player_id,min(event_date) first_login from activity
 group by player_id
