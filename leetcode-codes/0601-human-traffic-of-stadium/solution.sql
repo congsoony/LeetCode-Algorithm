62ebf30a59da24a07eaf5809bf44b2b8445b0a3d
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-20 01:21:33 +0000

    Sync LeetCode submission Runtime - 393 ms (48.19%), Memory - 0.0B (100.00%)

--- leetcode-codes/0601-human-traffic-of-stadium/solution.sql
+++ leetcode-codes/0601-human-traffic-of-stadium/solution.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
+with temp as(select id, visit_date,lag(people) over() prev,people,lead(people) over() next ,
+lag(people,2) over() prev2,lead(people,2) over() next2
+from stadium)
 
-select id, visit_date,people 
-from
-(select id, visit_date,people ,lead(people,1) over(order by id) nxt1 ,lead(people,2) over(order by id) nxt2,
-lag(people,1) over(order by id) b1,
-lag(people,2) over(order by id) b2
-from stadium) as a
-where (people>=100 and nxt1>=100 and nxt2>=100) || (people>=100 and b1>=100 and nxt1>=100) || (people>=100 and b1>=100 and b2>=100)
+select id,visit_date,people
+from temp
+where (prev>=100 and prev2 >= 100 and people>=100) or (prev>=100 and people>=100 and next>=100) or (people>=100 and next>=100 and next2>=100)
 order by visit_date
+
+
