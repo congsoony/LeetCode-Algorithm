de6285188cb653eafbb834e494350b116499289a
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-23 00:28:59 +0000

    Sync LeetCode submission Runtime - 464 ms (84.57%), Memory - 0.0B (100.00%)

--- leetcode-codes/1278-product-price-at-a-given-date/solution.sql
+++ leetcode-codes/1278-product-price-at-a-given-date/solution.sql
@@ -1,9 +1,9 @@
+# Write your MySQL query statement below
+with temp as(select product_id, new_price, change_date,
+dense_rank() over(partition by product_id order by change_date desc) as 'ran'
+from products
+where change_date <='2019-08-16')
 
-select product_id,first_value(new_price) over(partition by product_id order by change_date desc) price
-from products
-where change_date<= '2019-08-16'
-union
-select product_id,10
-from products
-group by product_id
-having min(change_date)>'2019-08-16'
+select p.product_id,ifnull(t.new_price,10) price from (select * from products group by product_id) p
+left join temp t on p.product_id = t.product_id and t.ran = 1
+
