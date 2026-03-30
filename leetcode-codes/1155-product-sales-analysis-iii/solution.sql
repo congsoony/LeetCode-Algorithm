0ca17ce6296788a252f60279ef49209827f23079
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-30 01:10:08 +0000

    Sync LeetCode submission Runtime - 659 ms (99.48%), Memory - 0.0B (100.00%)

--- leetcode-codes/1155-product-sales-analysis-iii/solution.sql
+++ leetcode-codes/1155-product-sales-analysis-iii/solution.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
-select s.product_id,s.year first_year,s.quantity,s.price
-from sales s
-where (s.product_id,s.year) in (select product_id,min(year) year from sales
+select s1.product_id,s1.year first_year,s1.quantity,s1.price
+from sales s1
+where (s1.product_id,s1.year) in
+(select product_id, min(year) year
+from sales
 group by product_id)
+
