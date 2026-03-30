26f44e3e9dbb4ec212aad59a66e5c480b9f2e058
congsoony <44600941+congsoony@users.noreply.github.com>
2026-03-30 01:36:51 +0000

    Sync LeetCode submission Runtime - 1084 ms (5.02%), Memory - 0.0B (100.00%)

--- leetcode-codes/0626-exchange-seats/solution.sql
+++ leetcode-codes/0626-exchange-seats/solution.sql
@@ -1,11 +1,4 @@
 # Write your MySQL query statement below
-select case when (select count(*) num from seat) %2 =1 then 
-case when id =(select count(*) num from seat)  then id
-when id%2=1 then id+1
-else id-1
-end
-when id%2=1 then id+1
-else id-1
- end as id,
-student from seat
-order by id
+select id, ifnull(if(id%2=1,lead(student) over(order by id),lag(student) over(order by id)),student) student
+from seat
+
