88ee8081a6f13c5dae5ba6ab486ff3da35b7f35f
congsoony <44600941+congsoony@users.noreply.github.com>
2026-07-07 05:57:25 +0000

    Sync LeetCode submission Runtime - 455 ms (44.56%), Memory - 0.0B (100.00%)

--- leetcode-codes/0619-biggest-single-number/solution.sql
+++ leetcode-codes/0619-biggest-single-number/solution.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
-select (select num from mynumbers
+select max(num) num from mynumbers
+where num  = (select num from mynumbers
 group by num
-having count(num)=1
+having count(num) = 1
 order by num desc
-limit 1) as num
+limit 1)
