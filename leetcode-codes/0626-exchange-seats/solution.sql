7d6a1f44ecf949983a14ddaa7fd65a3eebb9a752
congsoony <44600941+congsoony@users.noreply.github.com>
2026-07-07 06:48:04 +0000

    Sync LeetCode submission Runtime - 362 ms (56.19%), Memory - 0.0B (100.00%)

--- leetcode-codes/0626-exchange-seats/solution.sql
+++ leetcode-codes/0626-exchange-seats/solution.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
-select id, ifnull(if(id%2=1,lead(student) over(order by id),lag(student) over(order by id)),student) student
+select id, ifnull(if (id%2=1,
+lead(student) over(order by id),
+lag(student) over(order by id)
+),student)student
 from seat
-
