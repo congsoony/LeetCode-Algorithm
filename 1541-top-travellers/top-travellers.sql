02dacc10a367721356fab8cdfee9944e0a30f430
congsoony <44600941+congsoony@users.noreply.github.com>
2026-07-15 12:47:59 +0900

    Time: 810 ms (89.03%) | Memory: 0B (100.00%) - LeetSync

--- 1541-top-travellers/top-travellers.sql
+++ 1541-top-travellers/top-travellers.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select u.name , ifnull(sum(r.distance),0) travelled_distance
+select u.name , coalesce(sum(r.distance),0) travelled_distance
 from users u 
 left join rides r on u.id = r.user_id
 group by u.id
