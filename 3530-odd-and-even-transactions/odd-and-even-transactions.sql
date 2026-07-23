477845fc6e789fd0965e50c4aa8b9b7e782996e3
congsoony <44600941+congsoony@users.noreply.github.com>
2026-07-23 16:38:27 +0900

    Time: 288 ms (74.68%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3530-odd-and-even-transactions/odd-and-even-transactions.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+select transaction_date ,sum(if(amount%2=1,amount,0)) odd_sum ,sum(if(amount%2=0,amount,0)) even_sum from transactions
+group by transaction_date
+order by transaction_date
